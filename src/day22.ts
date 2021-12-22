73c88a2ab9bd923d9b5901b7c2e862956864c2d7
Daniel Mayorga <danielmayorga@daniels-mbp.lan>
2021-12-21 23:22:41 -0800

    complete part 2

--- src/day22.ts
+++ src/day22.ts
@@ -64,8 +64,89 @@ function part1(instructions: Instruction[]){
     return set.size;
 }
 
+interface Cube{
+    x: Range;
+    y: Range;
+    z: Range;
+}
+
 function part2(instructions: Instruction[]){
-    //todo
+    //thought in 3d for a bit and came up with this janky solution
+    let cubeSpace: Cube[] = [];
+    let removedCubeSpace: Cube[] = [];
+
+    const intersectRange = (a: Range, b: Range) => a.start <= b.end && a.end >= b.start;
+    const isOverlapCube = (a: Cube, b: Cube) => 
+        intersectRange(a.x, b.x) &&
+        intersectRange(a.y, b.y) &&
+        intersectRange(a.z, b.z);
+
+    function interceptRange(a:Range,b: Range):Range{
+        return ({
+            start: Math.max(b.start, a.start),
+            end: Math.min(b.end, a.end),
+        });
+    }        
+
+    const getOverlapCube = (a: Cube, b: Cube) => 
+        ({
+            x: interceptRange(a.x, b.x),
+            y: interceptRange(a.y, b.y),
+            z: interceptRange(a.z, b.z),
+        } as Cube) ;
+
+    for(let {on, xRange: x, yRange: y, zRange: z} of instructions){
+        let current = {x,y,z};
+        if (on){
+            //find overlap with existing cubespace and remove the overlapping area
+            let currentOverlap = []
+            for(let cube of cubeSpace){
+                if (isOverlapCube(current,cube)){
+                    currentOverlap.push(getOverlapCube(current,cube));
+                }
+            }
+            //find overlap with deleted elements and readd them to the cubespace
+            for(let removed of removedCubeSpace){
+                if(isOverlapCube(current, removed)){
+                    cubeSpace.push(getOverlapCube(current, removed));
+                }
+            }
+            //add your own overlaps to the remove pile 
+            removedCubeSpace.push(...currentOverlap);
+            // add to the current area to the cubespace
+            cubeSpace.push(current);
+        }else{
+            //remove elements you are currently overlapping with in the cubespace
+            let currentRemoveOverlap = [];
+            for(let cube of cubeSpace){
+                if(isOverlapCube(current, cube)){
+                    currentRemoveOverlap.push(getOverlapCube(current, cube));
+                }
+            }
+            //if you overlap with an element in the removed space, readd that space to prevent double removal
+            for(let removed of removedCubeSpace){
+                if(isOverlapCube(current, removed)){
+                    cubeSpace.push(getOverlapCube(current,removed));
+                }
+            }
+            //remove overlapping elements from the cubespace
+            removedCubeSpace.push(...currentRemoveOverlap);
+        }
+    }
+
+    const area = (cube: Cube) => 
+        BigInt((cube.x.end - cube.x.start+1)*
+        (cube.y.end - cube.y.start+1)*
+        (cube.z.end - cube.z.start+1));
+
+    let result = 0n;
+    for (let space of cubeSpace.map(area)){
+        result+=space;
+    }
+    for (let removed of removedCubeSpace.map(area)){
+        result-=removed;
+    }
+    return result;
 }
 
 (async function(){
